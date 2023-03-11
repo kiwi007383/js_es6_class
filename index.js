1af04860b2aba4d97f8ecb283f15715a4bdcce13
kiwi <kiwi007383@gmail.com>
2023-03-11 21:30:32 +0630

    add subclass extends

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //constructor (build object)
 
 function person(name, age){
-    this.name = name;
+    this.name = name,
     this.age = age;
 };
 
@@ -13,7 +13,7 @@ console.log(person1);
 
 class Human{
     constructor(name, age){
-        this.name = name;
+        this.name = name,
         this.age = age;
     }
     static getAge(){
@@ -23,3 +23,33 @@ class Human{
 
 let human1 = new Human("red", 30);
 console.log(Human.getAge());
+
+//extend class subclass!!
+
+class Parent{
+    constructor(mother, father){
+        this.mother = mother,
+        this.father = father;
+    }
+    greeting(){
+        console.log('Hello, world');
+    };
+};
+
+let parent1 = new Parent('mya', 'daung');
+
+//console.log(parent1);
+
+class Children extends Parent{
+    constructor(mother, father, childName){
+        //super => parent
+        //sub => children
+
+        super(mother, father);
+        this.childName = childName;
+    }
+};
+
+let child1 = new Children('mya', 'daung', 'zayn');
+console.log(child1);
+child1.greeting();
